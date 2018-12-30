ea1efcc222e12ee9c81614d22f1e313bec571b4c
unsortedtosorted <44731492+unsortedtosorted@users.noreply.github.com>
2018-12-29 20:52:38 -0500

    Create generateParenthesis.py

---
+++ backtracking/generateParenthesis.py
@@ -0,0 +1,37 @@
+class Solution:
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        ans=[]
+        
+
+            
+        def valid(A):
+            bal=0
+            for x in A:
+                if bal<0:
+                    return False
+                if x=="(":
+                    bal+=1
+                else:
+                    bal-=1
+            return bal==0
+            
+        
+        def generate(A):
+            if len(A)==2*n:
+                if valid(A):
+                    ans.append("".join(A))
+            else:
+                A.append("(")
+                generate(A)
+                A.pop()
+                A.append(")")
+                generate(A)
+                A.pop()
+
+        generate([])
+        return ans
+        
